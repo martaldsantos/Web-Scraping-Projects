17a81393e61db65f0eccccf615c9623a240667fd
Marta dos Santos <marta.aldsantos@gmail.com>
2023-04-06 13:56:41 +0100

    added comments for the code

--- BaseGov/Requests/anuncios_request.py
+++ BaseGov/Requests/anuncios_request.py
@@ -4,10 +4,14 @@ import pandas as pd
 import time
 
 def get_data():
-    url = "https://www.base.gov.pt/Base4/pt/resultados/"
-    headers = {
+
+    #url to be scraped
+    url = "https://www.base.gov.pt/Base4/pt/resultados/" 
+    
+    #headers to be sent with the request
+    headers = { 
         'Host': 'www.base.gov.pt',
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/111.0',
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36 Edg/111.0.1661.62',
         'Accept': 'text/plain, */*; q=0.01',
         'Accept-Language': 'pt-PT,pt;q=0.8,en;q=0.5,en-US;q=0.3',
         'Accept-Encoding': 'gzip, deflate, br',
@@ -27,32 +31,38 @@ def get_data():
     }
 
 
-    payload = {
+    payload = { 
         'type': 'search_anuncios',
-        'version': '111.0',
-        'query': 'tipoacto=0&tipomodelo=0&tipocontrato=0',
-        'sort': '-drPublicationDate',
-        'size': '25'
+        'version': '111.0', #version of the search API being used
+        'query': 'tipoacto=0&tipomodelo=0&tipocontrato=0', #search query
+        'sort': '-drPublicationDate', #sorting order of the search results
+        'size': '25' #maximum number of search results to return. It will return (up) to 25 results because our webpage has 25 rows
     }
 
     i=0
     while True:
-        payload['page'] = i
-        response = requests.post(url, headers=headers, data=payload)
-        data = json.loads(response.content)
-        data = data['items']
+        payload['page'] = i #set the page number to be scraped
+        response = requests.post(url, headers=headers, data=payload) #send the request
+        data = json.loads(response.content) #get the response
+        data = data['items'] #get the items from the response in json format
         
         if i==0:
-            df = pd.DataFrame.from_dict(data)
+            df = pd.DataFrame.from_dict(data) #first dataframe creation
         else:
-            df2 = pd.DataFrame.from_dict(data)
-            df = pd.concat([df, df2], ignore_index=True)
+            df2 = pd.DataFrame.from_dict(data) #set data in dataframe format (helps with cleaning the utf-8 characters)
+            df = pd.concat([df, df2], ignore_index=True) #add data to the originally created dataframe
         
-        if i==9000: #Number of Pages to be Scraped
+        if i==10: #Number of pages to be scraped
             break
+        
         i+=1
-        time.sleep(2)
-        print(i)
+
+        time.sleep(2) #to avoid detection by the website
+
+        #code to see the number of the page that is being scraped
+        #print(i) 
+
+    #take a look at the data you're scraping
     print(df)
 
 if __name__ == "__main__":
